f65e86606f170979480a7678597174e19cbd4a41
unknown <accraiders@gmail.com>
2025-11-26 04:42:33 +0000

    Refactor: Switch to @libsql/client, remove better-sqlite3 config

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
   images: {
     domains: ['p16-sign-va.tiktokcdn.com', 'p77-sign-va.tiktokcdn.com'],
   },
-  webpack: (config) => {
-    config.externals.push('better-sqlite3');
-    return config;
-  },
 }
 
 module.exports = nextConfig
